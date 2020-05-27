ae2888e3580228353c6ad1931c23f877fd2de20a
SeoYurim <33304898+yurim22@users.noreply.github.com>
2020-05-27 20:57:44 +0900

    Delete h_index.py

--- h_index.py
+++
@@ -1,8 +0,0 @@
-def solution(citations):
-  for h in range(max(citations), 0, -1):
-    over_h = 0
-    for citation in citations:
-      if citation >= h:
-        over_h += 1
-         if over_h == h:
-         return h
